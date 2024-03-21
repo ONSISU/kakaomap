36e122da946eb40afeac57ce6b5728b3279c5d3d
남병현 <kmi5063@naver.com>
2024-03-21 09:11:16 +0900

    커밋

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <KakaoMap/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import KakaoMap from './components/KakaoMap.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    KakaoMap
   }
 }
 </script>
